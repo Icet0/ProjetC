11e5f8d6793bd41657417dce067cb55fc7a40b3f
Icet0 <49767388+Icet0@users.noreply.github.com>
2020-12-05 17:47:26 +0100

    ok

---
+++ ProjetC/Arbre_binaire.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef __ARBRE_BINAIRE__
+#define __ARBRE_BINAIRE__
+
+typedef int Elt;
+//typedef int bool;
+
+struct znoeud {
+	Elt elt;
+	struct znoeud* fils_gauche;
+	struct znoeud* fils_droit;
+};
+typedef struct znoeud noeud;
+typedef struct znoeud* arbre;
+
+// Type abstrait de données
+
+arbre creer_arbre_vide(void);
+arbre creer_arbre(Elt e, arbre fg, arbre fd);
+arbre fils_gauche(arbre a);
+arbre fils_droit(arbre a);
+Elt racine(arbre a);
+
+bool est_arbre_vide(arbre a);
+
+// Fonctions outils
+
+arbre creer_feuille(Elt e);
+bool est_feuille(arbre a);
+
+void print_arbre(arbre a);
+void free_noeud(arbre a);
+void free_arbre(arbre a);
+
+
+//PROCEDURES RAJOUTEES
+void print_noeud(arbre a);
+void parcours_profondeur(arbre a);
+
+#endif
--- ProjetC/main.cpp
+++ ProjetC/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//include de nos fichiers de ressources
+#include "Arbre_binaire.h"
+#include "Gestion_fichier.h"
+
 
 void main() {
 	printf("Hello World");

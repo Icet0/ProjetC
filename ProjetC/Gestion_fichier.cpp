260bf055c9d715f9e3c66172b04cbb3e84d9c6d1
Icet0 <49767388+Icet0@users.noreply.github.com>
2020-12-07 13:18:37 +0100

    ouverture_fichier_bit

--- ProjetC/Gestion_fichier.cpp
+++ ProjetC/Gestion_fichier.cpp
@@ -1,5 +1,30 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include "Arbre_binaire.h"
+#include "Gestion_fichier.h"
+
+
+//OUVERTURE D'UN FICHIER BIT A BIT
+
+
+Bin_file* ouverture_fichier_bit(char* nom_de_fichier, char mode) {
+
+	//Variable locale : fichier(pointeur sur Bin_file)
+	Bin_file* fichier;
+	fichier = (Bin_file*)malloc(sizeof(Bin_file));
+	fichier->mode = mode;
+	//Si accès en lecture
+	//Ouverture du fichier input en lecture
+	if (fichier->mode = 'r') {
+		fichier->file = fopen(nom_de_fichier, "input");
+	}
+	else if(fichier->mode = 'w'){
+		fichier->file = fopen(nom_de_fichier, "input");
+	}
+	fichier->record_length = 0;
+	fichier->i_record = 0;
+	fichier->i_octet = 0;
+	fichier->nb_octets = 0;
+	return fichier;
+}
--- ProjetC/Gestion_fichier.h
+++ ProjetC/Gestion_fichier.h
@@ -1,1 +1,12 @@
-#pragma once
+#define BLOCK_SIZE 4096 // 1 bloc sur disque = 4096 octets
+
+typedef struct {
+	FILE* file; // identification du fichier
+	char mode; // lecture 'r' ou écriture 'w'
+	unsigned char record[BLOCK_SIZE]; // tampon pour lire ou écriture
+	int record_length; // nombre d'éléments dans le tampon
+	int i_record; // indice dans le tampon
+	char octet[8]; // pour expension d'un octet en 8 caractères
+	int i_octet; // indice dans octet
+	int nb_octets; // nombre d'octets lus ou écrits
+} Bin_file;
